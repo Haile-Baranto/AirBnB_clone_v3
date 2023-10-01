0876e4678e3d95ebb8e16a0e11f2aa33d49e456f
Haile-Baranto <hailebaranto@gmail.com>
2023-10-01 15:32:49 +0300

    Add __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,7 +10,7 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 # Import routes from other files here
 # from api.v1.views.amenities import *
-# from api.v1.views.cities import *
+from api.v1.views.cities import *
 from api.v1.views.index import *
 # from api.v1.views.places_amenities import *
 # from api.v1.views.places import *
